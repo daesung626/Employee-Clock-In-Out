84497c8bc93db4c81731829c7ed962ff5aa1faa7
daesung626 <57124915+daesung626@users.noreply.github.com>
2020-06-06 03:21:29 +0900

    6/6
    Added login, graph, report, settings html

--- clockinout/app_one/urls.py
+++ clockinout/app_one/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('graph', views.graph),
+    path('report', views.report),
+    path('settings', views.settings),
+    path('login', views.login),
 ]
--- clockinout/app_one/views.py
+++ clockinout/app_one/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return render(request, "index.html")
+
+def graph(request):
+    return render(request, "graph.html")
+
+def report(request):
+    return render(request, "report.html")
+
+def settings(request):
+    return render(request, "settings.html")
+
+def login(request):
+    return render(request, "login.html")
